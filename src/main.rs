3acacbb7b24ef464fdb18bd3234173a48f4e0fed
koyasaeki <koyasaeki@gmail.com>
2023-11-20 18:05:12 +0900

    update: delta さんのテストコードを入れる

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
 fn main() {
     println!("Hello, world!");
 }
+
+trait BTree {
+    type Item;
+
+    // 新しいB-Treeを作成する
+    fn new() -> Self;
+
+    // B-Treeに要素を挿入する
+    fn insert(&mut self, item: Self::Item);
+
+    // 指定された要素がB-Treeに含まれているか確認する
+    fn contains(&self, item: &Self::Item) -> bool;
+
+    // B-Treeから要素を削除する
+    fn remove(&mut self, item: &Self::Item);
+
+    // B-Treeの要素数を返す
+    fn size(&self) -> usize;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct SimpleBTree {
+        // B-Treeのデータ構造をここに実装します
+    }
+
+    impl BTree for SimpleBTree {
+        type Item = i32;
+
+        fn new() -> Self {
+            // 新しいB-Treeのインスタンスを作成する
+        }
+
+        fn insert(&mut self, item: Self::Item) {
+            // 要素を挿入するロジックを実装する
+        }
+
+        fn contains(&self, item: &Self::Item) -> bool {
+            // 要素が含まれているか確認するロジックを実装する
+        }
+
+        fn remove(&mut self, item: &Self::Item) {
+            // 要素を削除するロジックを実装する
+        }
+
+        fn size(&self) -> usize {
+            // B-Treeのサイズを返すロジックを実装する
+        }
+    }
+
+    #[test]
+    fn test_insert() {
+        let mut tree = SimpleBTree::new();
+        tree.insert(5);
+        assert!(tree.contains(&5));
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut tree = SimpleBTree::new();
+        tree.insert(5);
+        tree.remove(&5);
+        assert!(!tree.contains(&5));
+    }
+
+    #[test]
+    fn test_size() {
+        let mut tree = SimpleBTree::new();
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(8);
+        assert_eq!(tree.size(), 3);
+    }
+}
